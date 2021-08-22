76bef27d325c4c325c09296e75664b5b902ed5ae
Vagrant User <vagrant@multicloud>
2021-08-22 19:29:33 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Laboratorio Pipeline DevOps"
+    return "Laboratorio Pipeline DevOps 1"
 
 if __name__ == '__main__':
     app.run(debug=True)
